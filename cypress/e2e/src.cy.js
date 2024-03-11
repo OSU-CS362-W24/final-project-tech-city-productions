a5a5245ad402993ddc4b855cf19623add0d674e0
中西勘太郎 <nakanishikantarou@10-197-129-235.sds.oregonstate.edu>
2024-03-10 20:16:02 -0700

    New test added: Saving a chart to the 'gallery'

--- cypress/e2e/src.cy.js
+++ cypress/e2e/src.cy.js
@@ -5,6 +5,7 @@
 it('Chart is correctly generated', () => {
   //Visit Website...
   cy.visit('/')
+  cy.get("h1").should("exist")
   //Get link and click the link for Line to create a chart
   cy.findByRole("link", {name: "Line"}).click()
   //Assert that Cypress press the correct link.
@@ -26,6 +27,7 @@ it('Chart is correctly generated', () => {
 it('Chart data is maintained across pages', () => {
   //Visit Website...
   cy.visit('/')
+  cy.get("h1").should("exist")
   //Get link and click the link for Line to create a chart
   cy.findByRole("link", {name: "Line"}).click()
   cy.findByText("Line Chart Builder").should("exist")
@@ -65,4 +67,33 @@ it('Chart data is maintained across pages', () => {
   cy.checkValues("8", "9", "3", "15")
   cy.checkValues("10", "11", "4", "25")
   cy.checkValues("12", "13", "5", "40")
+})
+
+it('Saving a chart to the “gallery”', () => {
+  // Visit Website...
+  cy.visit('/')
+  cy.get("h1").should("exist")
+  // Get link and click the link for Line to create a chart
+  cy.findByRole("link", {name: "Line"}).click()
+  cy.findByText("Line Chart Builder").should("exist")
+  // Create a title
+  cy.createChart("Cats vs. Dogs", "Cats", "Dogs")
+  // Enter values
+  cy.addValues("FALSE", "4", "5", "1", "3")
+  cy.addValues("TRUE", "6", "7", "2", "7")
+  cy.addValues("TRUE", "8", "9", "3", "15")
+  cy.addValues("TRUE", "10", "11", "4", "25")
+  cy.addValues("TRUE", "12", "13", "5", "40")
+  cy.findByRole("button", {name: "+"}).click()
+  // Click generate chart button
+  cy.findByRole("button", {name: "Generate chart"}).click()
+  // Assert that the chart should exist.
+  cy.findByRole("img").should("exist")
+
+  // Save the chart in the gallery, jump to the gallery, and then assert the img exists in the gallery.
+  cy.findByRole("button", {name: "Save chart"}).click()
+  cy.findByRole("link", {name: "Gallery"}).click()
+  // Assert Cypress jump to Gallery
+  cy.get("h1").should("exist")
+  cy.findByRole("img").should("exist")
 })
